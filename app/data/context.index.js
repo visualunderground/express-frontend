8689d88cf9da3a419f34f34ff9f1ac25d4deb28a
visualunderground <tom@visualunderground.co.uk>
2015-02-16 16:13:18 +0000

    Moved form content  to template/js objs

--- app/data/context.index.js
+++ app/data/context.index.js
@@ -1,3 +1,165 @@
 module.exports = {
-  title: 'Express front end: Home'
+    
+    title: 'Express front end: Home',
+    
+    CONTEXT_MESSAGES: {
+        EL_MESSAGE: [
+            {mod: "master", content: "<h3>{.message--master}</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <a href='#'>link</a> Quisque sed fringilla ipsum. Suspendisse ut sem augue. Suspendisse sed magna a neque pretium aliquet. Sed justo dolor, bibendum vehicula imperdiet nec. </p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pharetra pellentesque Justo quis sodales. Suspendisse dolor magna a fringilla augue. Ut feugiat turpis a faselli.</p>"},
+            {mod: "error-up", content: "<p>{.message--error-up}<br>There are errors on this page please take a moment to check your answers.</p>"},
+            {mod: "error-down", content: "<p>{.message--error-down}<br>There are errors on this page please take a moment to check your answers.</p>"},
+            {mod: "info-up", content: "<p>{.message--info-up}<br>There are errors on this page please take a moment to check your answers.</p>"},
+            {mod: "info-down", content: "<p>{.message--info-down}<br>There are errors on this page please take a moment to check your answers.</p>"}
+        ]
+    },
+
+    CONTEXT_BLOCK_LINKS: {
+        EL_BLOCK_LINK: [
+            {title: "master", meta: "Whoa meta stuff"},
+            {title: "error-up", meta: "Whoa meta stuff"},
+            {title: "error-down", meta: "Whoa meta stuff"}
+        ]
+    },
+
+
+    CONTEXT_FORM: {
+
+        EL_ROW: [
+            {
+                EL_QUESTION: {text: "Input"},
+                EL_INPUT: {type: "text", placeholder: "[type='text']"}
+            },
+            {
+                error: "Oops. there was an error",
+                EL_QUESTION: {text: "Input"},
+                EL_INPUT: {type: "tel", placeholder: "[type='tel']"}
+            },
+            {
+                EL_QUESTION: {text: "Input"},
+                EL_INPUT: {type: "email", placeholder: "[type='email']"}
+            },
+            {
+                EL_QUESTION: {text: "Input"},
+                EL_INPUT: {type: "password", placeholder: "[type='password']"}
+            },
+            {
+                EL_QUESTION: {text: "search-suggest input"},
+                EL_INPUT: {type: "text", mod: "search", hint: "Additional information"}
+            },
+            {
+                EL_QUESTION: {text: "Textarea"},
+                EL_TEXTAREA: {}
+            },
+            {
+                EL_QUESTION: {text: "Input (prefixed)", hint: "Additional information with <a href='#''>link</a>"},
+                EL_INPUT: {type: "text", placeholder: "for example, 2.99", prefix: "£"}
+            },
+            {
+                EL_QUESTION: {text: "Input (suffixed)"},
+                EL_INPUT: {type: "text", placeholder: "for example, 0.5", suffix: "mg"}
+            },
+            {
+                EL_QUESTION: {text: "Textual value", hint: "Do not use disabled inputs"},
+                EL_FAUX: {text: "Master Stewie Griffin"}
+            },
+            {
+                EL_QUESTION: {text: "Delimited text input"},
+                EL_INPUT: [
+                    { mod: "xs", type: "text", placeholder: "yyyy", delimiter: "-"},
+                    { mod: "xs", type: "text", placeholder: "yyyy", delimiter: "-"},
+                    { mod: "xs", type: "text", placeholder: "yyyy"}
+                ]
+            },
+            {
+                EL_QUESTION: {text: "Selection (std. width)", hint: "Matches text input width"},
+                EL_SELECT: { 
+                    options: [
+                        {value: "1", text: "mobile"},
+                        {value: "2", text: "tablet"},
+                        {value: "3", text: "desktop"}
+                    ]
+                }
+            },
+            {
+                EL_QUESTION: {text: "Selection (matches. width)", hint: "A select element with a class of min = data width."},
+                EL_SELECT: {
+                    mod: "min",  
+                    options: [
+                        {value: "1", text: "mobile"},
+                        {value: "2", text: "tablet"},
+                        {value: "3", text: "desktop"}
+                    ]
+                }
+            },
+            {
+                EL_QUESTION: {text: "Radios"},
+                EL_TOUCH: {
+                    options: [
+                        { name: "radio", value: "1", type: "radio", text: "Opt 1"},
+                        { name: "radio", value: "2", type: "radio", text: "Opt 2"},
+                        { name: "radio", value: "3", type: "radio", text: "Opt 3"},
+                        { name: "radio", value: "4", type: "radio", text: "Opt 4"}
+                    ]
+                }
+            },
+            {
+                EL_QUESTION: {text: "Checkboxes", hint: "Please select all that apply"},
+                EL_TOUCH: {
+                    options: [
+                        { name: "checkbox", value: "1", type: "checkbox", text: "Opt 1"},
+                        { name: "checkbox", value: "2", type: "checkbox", text: "Opt 2"},
+                        { name: "checkbox", value: "3", type: "checkbox", text: "Opt 3"},
+                        { name: "checkbox", value: "4", type: "checkbox", text: "Opt 4"}
+                    ]
+                }
+            },
+
+            {
+                EL_QUESTION: {text: "Date format"},
+                // TODO: use GDS style fieldsets
+                EL_SELECT: [
+                    {mod: "min", noNull: true, options: [{value: "0", text: "Day"},{value: "1", text: "1"},{value: "etc.", text: "..."},{value: "2", text: "31"}]},
+                    {mod: "min", noNull: true, options: [{value: "0", text: "Month"},{value: "1", text: "Jan"},{value: "etc.", text: "..."},{value: "2", text: "Dec"}]},
+                    {mod: "min", noNull: true, options: [{value: "0", text: "Year"},{value: "1", text: "2015"},{value: "etc.", text: "..."},{value: "2", text: "1900"}]},
+                ]
+            },
+
+            {
+                EL_QUESTION: {text: "Date format - long year"},
+                // TODO: use GDS style fieldsets
+                EL_SELECT: [
+                    {mod: "min", noNull: true, options: [{value: "0", text: "Day"},{value: "1", text: "1"},{value: "etc.", text: "..."},{value: "2", text: "31"}]},
+                    {mod: "min", noNull: true, options: [{value: "0", text: "Month"},{value: "1", text: "Jan"},{value: "etc.", text: "..."},{value: "2", text: "Dec"}]}
+                ],
+                // TODO: slight hack here - it's relying on the order of partials in molecule_question.hbs
+                EL_INPUT: { mod: "xs", type: "text", placeholder: "yyyy"}
+            },
+            {
+                EL_QUESTION: {text: "Buttons"},
+                EL_BUTTON: [
+                    {text: "Button"},
+                    {text: "button--secondary", mod: "secondary"},
+                    {text: "button--tertiary", mod: "tertiary"},
+                    {text: "button--muted", mod: "muted"}
+                ]
+            },
+            {
+                EL_QUESTION: {text: "Input and search"},
+                // TODO: slight hack here - it's relying on the order of partials in molecule_question.hbs
+                EL_INPUT: {type: "text"},
+                EL_BUTTON: {text: "Search", mod: "secondary"}
+            }
+
+        ]
+
+    },
+
+    CONTEXT_NAV: {
+        EL_BUTTON: [
+            {text: "Back", mod: "muted"},
+            {text: "Continue"}
+        ]
+    }
+
 };
+
+        
